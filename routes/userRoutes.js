b933dc0f9381011d76d1f5b8bd89e9089e75a6ee
OstrichWhite <rahimcsestudies@gmail.com>
2022-06-17 00:39:59 +0530

    create user model and signup user with hashed passwords

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,14 @@ const {
   getUser,
   updateUser,
   deleteUser,
-} = require('./../controllers/userController');
+} = require('../controllers/userController');
+
+const { signUp } = require('../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', signUp);
+
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
