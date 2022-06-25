5857e0f041bc36529f14cf39967875bfacf1e775
OstrichWhite <rahimcsestudies@gmail.com>
2022-06-26 04:48:33 +0530

    Updating and Deleting the Current User, Send jwt via httpOnly cookie, Implementing rate limiting, set Security HTTP Headers by helmet, Data sanitization against NoSQL query injection and XSS, preventing parameter pollution

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // node_modules
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp'); //http parameter pollution
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,13 +14,51 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+//GLOBAL MIDDLEWARES
+//set Security HTTP header
+app.use(helmet()); //function call do middleware action
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-app.use(express.json()); //used to parse the incoming requests with JSON payloads
+
+// Limit request from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please Try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); //for incoming requests with JSON payloads
+
+// Data sanitization against NoSQL query injection // {"email": {"$ge":""}}
+app.use(mongoSanitize()); // it remove mongoose operator like $
+
+// Data sanitization against XSS
+app.use(xss()); //it convert html syntax elements like '<' to &lt;
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+); //remove duplicate parameters
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
   next();
 });
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
-
   res.status(200).json({
     status: 'success',
     results: users.length,
@@ -12,6 +20,37 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs a password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password update', 400));
+  }
+  // 2) Filter Out the unwanted fields that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  return res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
